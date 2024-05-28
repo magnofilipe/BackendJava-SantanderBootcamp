c46960e0ca4cc5684299012294efc811a516e529
magnofilipe <filipemagno.contato@gmail.com>
2024-05-28 16:17:54 -0300

    feat: resolucao do desafio "CarrinhoDeCompras"

--- src/bootcamp/collectionschallenges/list/compras/Main.java
+++ src/bootcamp/collectionschallenges/list/compras/Main.java
@@ -1,4 +1,24 @@
 package bootcamp.collectionschallenges.list.compras;
 
 public class Main {
+
+    public static void main(String[] args) {
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
+        System.out.println(carrinhoDeCompras.calcularValorTotal());
+
+        carrinhoDeCompras.adicionarItem("Tomate", 3.50, 2);
+        carrinhoDeCompras.adicionarItem("Leite em pó", 3.0, 1);
+        carrinhoDeCompras.adicionarItem("Tomate", 3.50, 5);
+
+        carrinhoDeCompras.exibirItens();
+
+        System.out.println(carrinhoDeCompras.calcularValorTotal());
+
+        carrinhoDeCompras.removerItem("Tomate");
+
+        carrinhoDeCompras.exibirItens();
+    }
+
+
 }
