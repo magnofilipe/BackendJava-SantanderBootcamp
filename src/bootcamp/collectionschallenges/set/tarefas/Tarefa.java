9d9fdf22aadd199b467117defa439b6f6f06545e
magnofilipe <filipemagno.contato@gmail.com>
2024-05-29 14:23:09 -0300

    feat: resolucao do desafio "ListaTarefa"!

---
+++ src/bootcamp/collectionschallenges/set/tarefas/Tarefa.java
@@ -0,0 +1,46 @@
+package bootcamp.collectionschallenges.set.tarefas;
+
+import java.util.Objects;
+
+public class Tarefa {
+    private String descricao;
+    private boolean tarefaConcluida;
+
+    public Tarefa(String descricao, boolean tarefaConcluida) {
+        this.descricao = descricao;
+        this.tarefaConcluida = tarefaConcluida;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isTarefaConcluida() {
+        return tarefaConcluida;
+    }
+
+    public void setTarefaConcluida(boolean tarefaConcluida) {
+        this.tarefaConcluida = tarefaConcluida;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tarefa tarefa = (Tarefa) o;
+        return Objects.equals(descricao, tarefa.descricao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(descricao);
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa{" +
+                "descricao='" + descricao + '\'' +
+                ", tarefaConcluida=" + tarefaConcluida +
+                '}';
+    }
+}
