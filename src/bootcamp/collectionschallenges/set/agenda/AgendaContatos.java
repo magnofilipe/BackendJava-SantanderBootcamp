1d3b8351489e6eb6eeeb690c99d1b8c623da87dd
magnofilipe <filipemagno.contato@gmail.com>
2024-05-29 13:57:14 -0300

    feat: adicao do desafio "AgendaContatos"

---
+++ src/bootcamp/collectionschallenges/set/agenda/AgendaContatos.java
@@ -0,0 +1,49 @@
+package bootcamp.collectionschallenges.set.agenda;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero) {
+        contatoSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContatos() {
+        if (!contatoSet.isEmpty()) {
+            System.out.println(contatoSet);
+        } else {
+            System.out.println("Conjunto vazio.");
+        }
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome) {
+        Set<Contato> resultado = new HashSet<>();
+        for (Contato c : contatoSet) {
+            if (c.getNome().startsWith(nome)) {
+                resultado.add(c);
+            }
+        }
+
+        return resultado;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero) {
+        Contato contatoAtualizado = null;
+
+        for (Contato c : contatoSet) {
+            if (c.getNome().equalsIgnoreCase(nome)) {
+                c.setNumero(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+
+        return contatoAtualizado;
+    }
+}
